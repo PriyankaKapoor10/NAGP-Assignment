f02ba0bf22c6655bb1bab4e545205ded13fe8297
Priyanka Kapoor <priyanka.kapoor@nagarro.com>
2024-05-17 18:56:52 +0530

    First Commit

---
+++ node-project/routes/routes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const router = express.Router()
+
+module.exports = router;
+const Model = require('../models/model');
+
+
+//Post Method
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(401).json({message: error.message})
+    }
+})
+
+//Get all Method
+/* router.get('/getAll', (req, res) => {
+    res.send('Get All API')
+}) */
+
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get by ID Method
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
